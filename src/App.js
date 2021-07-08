89bba5ddde1c4099df2363b1a9cf5e5448d24b4f
D41152670 <thiago.mendes@adtalem.com>
2021-07-08 19:04:31 -0300

    Primeiras anotaçoes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+// Componentes stateLess é sem stado
+// Componentes stateFull é com stado
+class App extends Component 
+{
+  /* É possivel não utilziar o construtor daclarando o state diretamente */
+  /* constructor( props ) {
+    super( props );
+    // O bind pode ser alterado nas funções por arrow functions
+    this.handlePClick = this.handlePClick.bind(this);
+
+    this.state = {
+      name: 'Thiago Mendes',
+      counter: 0
+    }
+  }
+ */
+
+  state = {
+    name: "Thiago mendes",
+    counter: 0
+  }
+
+  // isso é uma arrow function
+  handlePClick = () =>
+  {
+    this.setState({name: 'Bom vamos ver'});
+  }
+
+  handleAClick = (event) => 
+  {
+    event.preventDefault();
+    const { counter } = this.state; 
+    this.setState( { counter: counter + 1 } );
+  }
+
+  render()
+  {
+    // Get the variable state
+    const { name } = this.state;
+    const { counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={ this.handlePClick }>
+            { name }
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            link { counter }
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +84,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
 export default App;

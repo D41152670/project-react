9f8db71b2667d8c6a325884223579ad1b7804cf7
D41152670 <thiago.mendes@adtalem.com>
2021-07-09 20:25:54 -0300

    projeto react Aula 16

--- src/App.js
+++ src/App.js
@@ -1,89 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
-// Componentes stateLess é sem stado
-// Componentes stateFull é com stado
+/* Main class extends component */
 class App extends Component 
 {
-  /* É possivel não utilziar o construtor daclarando o state diretamente */
-  /* constructor( props ) {
-    super( props );
-    // O bind pode ser alterado nas funções por arrow functions
-    this.handlePClick = this.handlePClick.bind(this);
-
-    this.state = {
-      name: 'Thiago Mendes',
-      counter: 0
-    }
-  }
- */
-
   state = {
-    name: "Thiago mendes",
-    counter: 0
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: "title 1",
+        body: "This is the body post 1"
+      },
+      {
+        id: 2,
+        title: "title 2",
+        body: "This is the body post 2"
+      },
+      {
+        id: 3,
+        title: "title 3",
+        body: "This is the body post 3"
+      },
+    ]
   }
 
-  // isso é uma arrow function
-  handlePClick = () =>
+  componentDidMount() 
   {
-    this.setState({name: 'Bom vamos ver'});
+    this.handleTimeOut();
   }
 
-  handleAClick = (event) => 
+  componentDidUpdate()
   {
-    event.preventDefault();
-    const { counter } = this.state; 
-    this.setState( { counter: counter + 1 } );
+    /* this.handleTimeOut(); */
+  }
+
+  handleTimeOut = () =>
+  {
+    // Get the variable state
+    const { posts, counter } = this.state;
+    posts[0].title = "olá um novo titulo";
+
+    setTimeout(() => {
+        this.setState({posts, counter: counter + 1});
+      }, 1000);
   }
 
   render()
   {
     // Get the variable state
-    const { name } = this.state;
-    const { counter } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={ this.handlePClick }>
-            { name }
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            link { counter }
-          </a>
-        </header>
+        { posts.map( post => (
+          <div key={post.id}>
+            <h1 > { post.title } </h1>
+            <div> {post.body} </div>
+            <div> { counter } </div>
+          </div>
+        )) }
       </div>
     );
   }
 }
-
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} */
 
 export default App;
